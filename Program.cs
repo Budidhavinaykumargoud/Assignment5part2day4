5e69c10f95decf97e8189ef020212ba261d76dc2
Budidhavinaykumargoud <vinaygoud911@gmail.com>
2023-11-09 15:16:47 +0530

    AssignLoops

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First 10 natural numbers:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine(); // Add a newline for better readability
+
+            // Using a while loop to print the first 10 even natural numbers
+            Console.WriteLine("First 10 even natural numbers:");
+            int evenNumber = 2;
+            int evenCount = 0;
+            while (evenCount < 10)
+            {
+                Console.Write(evenNumber + " ");
+                evenNumber += 2;
+                evenCount++;
+            }
+
+            Console.WriteLine(); // Add a newline for better readability
+
+            // Using a do-while loop to print the first 10 odd natural numbers
+            Console.WriteLine("First 10 odd natural numbers:");
+            int oddNumber = 1;
+            int oddCount = 0;
+            do
+            {
+                Console.Write(oddNumber + " ");
+                oddNumber += 2;
+                oddCount++;
+            } while (oddCount < 10);
+              Console.ReadLine();
+        }
+    }
+}
